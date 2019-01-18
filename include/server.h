f5679926537ed2210892c4fdd66eb7b0f12b2395
Michael Yu <michaelyu@cs130.org>
2019-01-18 23:31:27 +0000

    refactor session and server
    Change-Id: I9dab411915285248cfb6c5196aaebea61f4b2020

---
+++ include/server.h
@@ -0,0 +1,17 @@
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class session;
+
+class server {
+public:
+    server(boost::asio::io_service& io_service, short port);
+private:
+    void start_accept();
+    void handle_accept(session* new_session, 
+        const boost::system::error_code& error);
+    boost::asio::io_service& io_service_;
+    tcp::acceptor acceptor_;
+
+};
