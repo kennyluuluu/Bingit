c820c8a25d748da8c68b86369bef3f5b0d81e2bd
kluu <kluu@cs130.org>
2019-01-20 21:55:44 -0800

    handles requests
    Change-Id: I4d930ae8358cba9afe4a66a877de813d793502e1

--- src/session.cc
+++ src/session.cc
@@ -25,10 +25,72 @@ void session::handle_read(const boost::system::error_code &error,
 {
     if (!error)
     {
-        boost::asio::async_write(socket_,
-                                 boost::asio::buffer(data_, bytes_transferred),
-                                 boost::bind(&session::handle_write, this,
-                                             boost::asio::placeholders::error));
+	// PARSE Request-Line
+	// Request-Line = Method SP Request-URI SP HTTP-Version CRLF
+	int i = 0; // iterator for data_
+	std::string Method = "";
+	std::string Request_URI = "";
+	std::string HTTP_Version = "";
+	
+	while(i < bytes_transferred)	    	
+	{
+	    const char c = data_[i];
+	    i++;
+	    if (c == ' ')
+	    {
+	        // allowed methods
+		if(Method.compare("OPTIONS") != 0 &&
+		   Method.compare("GET") != 0 &&
+		   Method.compare("HEAD") != 0 &&
+		   Method.compare("POST") != 0 &&
+		   Method.compare("PUT") != 0 &&
+		   Method.compare("DELETE") != 0 &&
+		   Method.compare("TRACE") != 0 &&
+		   Method.compare("CONNECT") != 0 &&
+		   Method.compare("GET") != 0)
+		    return;	
+	        break; // END of Method
+	    }
+	    Method += c;
+	}
+
+	// missing URI, version, and CRLF
+	if (i >= bytes_transferred)
+	    return;
+	
+	while (i < bytes_transferred)
+	{
+	    const char c = data_[i];
+	    i++;
+	    if (c == ' ')
+	    {
+	        // invalid syntax
+		if (Request_URI.size() == 0)
+		{
+		    return;
+		}
+		break; // end of Request_URI
+	    }
+	    Request_URI += c;
+	}
+	
+	while (i < bytes_transferred)
+	{
+	    const char c = data_[i];
+	    i++;
+	    // valid syntax for request-line
+	    if (c == '\r\n' && HTTP_Version.size() > 0)
+	    {
+		// ECHO BACK REQUEST    
+		boost::asio::async_write(socket_,
+                	                 boost::asio::buffer(data_, bytes_transferred),
+                        	         boost::bind(&session::handle_write, this,
+                                           boost::asio::placeholders::error));
+		break;
+	    }
+	    HTTP_Version += c;
+	}
+	//fprintf(stderr, "HTTP_Version is %s", HTTP_Version.c_str()); DEBUG
     }
     else
     {
