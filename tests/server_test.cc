5cc487d369b7af35db603e2b408378f67c3c7c01
Michael Yu <michaelyu@cs130.org>
2019-01-25 01:07:43 +0000

    test skeleton code added
    Change-Id: I36c72834c062c2d4f85a6bb23940c1e6d58d138c

---
+++ tests/server_test.cc
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "server.h"
+
+TEST(ServerTest, CMakeTest)
+{
+    bool success = true;
+    EXPECT_TRUE(success);
+}
+
+// TODO: Add Test Fixture later
---
+++ tests/session_test.cc
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "session.h"
+
+TEST(SessionTest, CMakeTest)
+{
+    bool success = true;
+    EXPECT_TRUE(success);
+}
+
+// TODO: Add Test Fixture later
