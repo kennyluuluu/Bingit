4c519fb22c6a8490f101cc8c03d6780fb62404e8
kluu <kluu@cs130.org>
2019-01-24 19:26:53 -0800

    refactored handle_read for testing & improved parser
    Change-Id: I6dfa21336c5e6652c97b497e1337fa42d674662a

--- src/session.cc
+++ src/session.cc
@@ -20,79 +20,85 @@ void session::start()
                                         boost::asio::placeholders::bytes_transferred));
 }
 
+bool parse_request_line(char* request_line, size_t request_size)
+{
+    // Request-Line = Method SP Request-URI SP HTTP-Version CRLF
+    int iter = 0;  // iterator for request_line
+    std::string method = "";
+    std::string URI = "";
+    std::string HTTP_version = "";
+
+    while (iter < request_size)
+    {
+        const char c = request_line[iter];
+	iter++;
+	if (c == ' ')
+	{
+	    if (method.compare("GET") == 0)
+	    {
+	         break;
+	    }
+	    else return false;
+	}
+	method += c;
+    }
+
+    while (iter < request_size)
+    {
+        const char c = request_line[iter];
+	iter++;
+	if (c == ' ')
+	{
+	    // parse URI format
+	    if (URI.size() > 0)
+	    {
+	        break;
+	    }
+	    else return false;
+	}
+	URI += c;
+    }
+
+    while (iter < request_size)
+    {
+        const char c = request_line[iter];
+	iter++;
+	if (c == '\r' && request_line[iter] == '\n')
+	{
+	    // check formatting for HTTP
+	    if (HTTP_version.compare(0, 5, "HTTP/") != 0) return false;
+            int count = 0;
+	    for (std::string::size_type i = 5; i < HTTP_version.size(); i++)
+	    {
+	        if (!isdigit(HTTP_version[5])) return false; // version cannot begin with .
+		if (HTTP_version[i] == '.')  // count the nummber of .
+		{
+		    count++;
+		    continue;
+		}
+                if (!isdigit(HTTP_version[i])) return false;
+	    }
+	    if (HTTP_version[HTTP_version.size() - 1] == '.') return false; // version cannot end with .
+	    if (count != 1) return false; // only 1 decimal allowed in version
+        }
+	HTTP_version += c;
+    }
+
+    // no errors detected
+    return true;
+}
+
+
 void session::handle_read(const boost::system::error_code &error,
                           size_t bytes_transferred)
 {
     if (!error)
     {
-	// PARSE Request-Line
-	// Request-Line = Method SP Request-URI SP HTTP-Version CRLF
-	int i = 0; // iterator for data_
-	std::string Method = "";
-	std::string Request_URI = "";
-	std::string HTTP_Version = "";
-	
-	while(i < bytes_transferred)	    	
-	{
-	    const char c = data_[i];
-	    i++;
-	    if (c == ' ')
-	    {
-	        // allowed methods
-		if(Method.compare("OPTIONS") != 0 &&
-		   Method.compare("GET") != 0 &&
-		   Method.compare("HEAD") != 0 &&
-		   Method.compare("POST") != 0 &&
-		   Method.compare("PUT") != 0 &&
-		   Method.compare("DELETE") != 0 &&
-		   Method.compare("TRACE") != 0 &&
-		   Method.compare("CONNECT") != 0 &&
-		   Method.compare("GET") != 0)
-		    return;	
-	        break; // END of Method
-	    }
-	    Method += c;
-	}
-
-	// missing URI, version, and CRLF
-	if (i >= bytes_transferred)
-	    return;
-	
-	while (i < bytes_transferred)
-	{
-	    const char c = data_[i];
-	    i++;
-	    if (c == ' ')
-	    {
-	        // invalid syntax
-		if (Request_URI.size() == 0)
-		{
-		    return;
-		}
-		break; // end of Request_URI
-	    }
-	    Request_URI += c;
-	}
-	
-	while (i < bytes_transferred)
-	{
-	    const char c = data_[i];
-	    i++;
-	    // valid syntax for request-line
-        
-	    if (c == '\r' && data_[i] == '\n')
-	    {
-            // check formatting for HTTP
-            if (HTTP_Version.compare(0, 5, "HTTP/") != 0) return;
-            for (std::string::size_type i = 5; i < HTTP_Version.size(); i++)
-            {
-                if (!isdigit(HTTP_Version[i]) && HTTP_Version[i] != '.')
-                    return;
-            }
-            
+	if (parse_request_line(data_, bytes_transferred))
+    	{
             // send response
             // generate status code and content type headers
-            std::string status_line = HTTP_Version + " 200 OK\r\n";
+            std::string status_line = "HTTP/1.1 200 OK\r\n";
             std::string content_type = "Content-Type: text/plain\r\n";
             std::string status_and_content = status_line + content_type;
 
@@ -105,10 +111,7 @@ void session::handle_read(const boost::system::error_code &error,
             boost::asio::async_write(socket_,
                                     boost::asio::buffer(response, bytes_transferred + status_and_content.size()),
                                     boost::bind(&session::handle_write, this,
-                                                boost::asio::placeholders::error));
-		break;
-	    }
-	    HTTP_Version += c;
+                                                boost::asio::placeholders::error));    
 	}
     }
     else
