6f1170ed6a9a41f0e74581f2b68e5bc6fa04c88b
Michael Yu <michaelyu@cs130.org>
2019-01-26 19:33:07 +0000

    added server unit tests
    Change-Id: Ic9ca2fde8b8e3698b84025b5025664c6b2e48d16

--- tests/server_test.cc
+++ tests/server_test.cc
@@ -1,10 +1,26 @@
 #include "gtest/gtest.h"
 #include "server.h"
 
-TEST(ServerTest, CMakeTest)
-{
-    bool success = true;
-    EXPECT_TRUE(success);
-}
+// TEST(ServerTest, CMakeTest)
+// {
+//     bool success = true;
+//     EXPECT_TRUE(success);
+// }
 
-// TODO: Add Test Fixture later
+class ServerTest : public ::testing::Test
+{
+  protected:
+    void SetUp() override
+    {
+    }
+
+    short port_num = 8080;
+    boost::asio::io_service io_service;
+    boost::asio::io_service *io_service_ptr = &io_service;
+};
+
+TEST_F(ServerTest, SimpleWorkingTest)
+{
+    server my_server(*io_service_ptr, port_num);
+    EXPECT_NE(io_service_ptr, nullptr);
+}
