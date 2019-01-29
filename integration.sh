2c1e725e22eae7257331697cb671f952f1da3987
Edwin Do <edwin.do@cs130.org>
2019-01-29 07:26:28 +0000

    updated integration test script
    Change-Id: I520db51739bbfb681f16c58528cdd34f471053ad

--- integration.sh
+++ integration.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # start webserver
-./build/bin/server ./configs/8080_config &
-id=$?
+./bin/server ../configs/8080_config &
+id=$!
 
 expected=$'GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: curl/7.58.0\r\nAccept: */*\r\n\r'
 
 # test
 generated_output=$(curl -sS localhost:8080)
 
-#echo $generated_output
 if [ "$expected" == "$generated_output" ]; then
     echo "Test Passed"
+    kill -s SIGINT $id
 else
     echo "Test Failed"
+    kill -s SIGINT $id
+    exit 1
 fi
 
-# stop webserver
-kill -s SIGINT $id
